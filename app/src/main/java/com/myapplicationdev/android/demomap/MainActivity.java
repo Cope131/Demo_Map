f8b479a6c6440c2afd80886b97e83d74d6d091e3
Daryl <darylgialolo@gmail.com>
2021-07-06 12:13:38 +0800

    2nd Commit

--- app/src/main/java/com/myapplicationdev/android/demomap/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demomap/MainActivity.java
@@ -2,22 +2,36 @@ package com.myapplicationdev.android.demomap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
 
     private final String DEBUG_TAG = MainActivity.class.getSimpleName();
+    private final int MY_LOCATION_REQUEST_CODE = 0;
 
     // Views
     private Button normalBtn, terrainBtn, satelliteBtn;
     private GoogleMap googleMap;
+    private boolean enableMyLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initViews();
         initMap();
+        askPermission();
+    }
+
+    private void addMarkers() {
+        // Locations
+        LatLng causewayPoint = new LatLng(1.436065, 103.786263);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(causewayPoint, 15));
+        LatLng rp = new LatLng(1.44224, 103.785733);
+
+        // Causeway Point Marker
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(causewayPoint)
+                .title("Causeway Point")
+                .snippet("Shopping after class")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        googleMap.addMarker(markerOptions);
+
+        // Republic Polytechnic Marker
+        markerOptions = new MarkerOptions()
+                .position(rp)
+                .title("Republic Polytechnic")
+                .snippet("C347 Android Programming II")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
+        googleMap.addMarker(markerOptions);
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
     }
 
     private void initMap() {
@@ -63,5 +106,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.googleMap = googleMap;
+        if (googleMap != null) {
+            addMarkers();
+            UiSettings uiSettings = googleMap.getUiSettings();
+            uiSettings.setCompassEnabled(true);
+            uiSettings.setZoomControlsEnabled(true);
+            uiSettings.setZoomGesturesEnabled(true);
+            uiSettings.setMyLocationButtonEnabled(true);
+            uiSettings.setRotateGesturesEnabled(true);
+
+            int permissionResult = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+            if (permissionResult == PermissionChecker.PERMISSION_GRANTED) {
+                googleMap.setMyLocationEnabled(enableMyLocation);
+            } else {
+                askPermission();
+            }
+        }
     }
 }
